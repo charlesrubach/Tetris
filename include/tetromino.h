cedb734e7ba20db0b5f200b51c205bc0eb78b469
Charles Rubach <charlesrubach@gmail.com>
2026-08-07 20:43:22 -0500

    Tetromino functions should be done unless something new comes up

--- include/tetromino.h
+++ include/tetromino.h
@@ -5,4 +5,41 @@
 #ifndef TETRIS_TETROMINO_H
 #define TETRIS_TETROMINO_H
 
+#define TETROMINO_SIZE  4
+#include "utilities.h"
+
+typedef enum TetrominoShape {
+    TETROMINO_I,
+    TETROMINO_J,
+    TETROMINO_L,
+    TETROMINO_O,
+    TETROMINO_S,
+    TETROMINO_T,
+    TETROMINO_Z,
+    TOTAL_TETROMINOES
+} TetrominoShape;
+
+extern Color TetrominoColors[];
+
+typedef enum TetrominoRotationState {
+    DEGREE_0, DEGREE_90_CW, DEGREE_180, DEGREE_270_CCW, TOTAL_ROTATION_STATES
+} TetrominoRotationState;
+
+typedef enum TetrominoDirection {
+    CW, CCW
+} TetrominoDirection;
+
+typedef struct Tetromino {
+    TetrominoShape grid[TETROMINO_SIZE][TETROMINO_SIZE];
+    TetrominoRotationState rotation_state;
+    TetrominoShape shape;
+    Color color;
+    Vector2i pos;
+} Tetromino_t;
+
+Tetromino_t *Tetromino_Create(const Game *game, TetrominoShape shape);
+void Tetromino_Rotate(Tetromino_t *tetromino, TetrominoDirection direction);
+void Tetromino_Move(Tetromino_t *tetromino);
+
+
 #endif //TETRIS_TETROMINO_H
--- src/tetromino.c
+++ src/tetromino.c
@@ -2,4 +2,126 @@
 // Created by Charles on 8/6/2026.
 //
 
-#include "../include/tetromino.h"
+#include <string.h>
+#include "game.h"
+
+const int tetromino_shape[TOTAL_TETROMINOES][TETROMINO_SIZE][TETROMINO_SIZE] = {
+
+    // Tetromino Shape I
+    {
+        {0, 0, 0, 0},
+        {1, 1, 1, 1},
+        {0, 0, 0, 0},
+        {0, 0, 0, 0}
+    },
+    // Tetromino Shape J
+    {
+            {1, 0, 0, 0},
+            {1, 1, 1, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+    },
+    // Tetromino Shape L
+    {
+            {0, 0, 1, 0},
+            {1, 1, 1, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+    },
+    // Tetromino Shape O
+    {
+            {0, 1, 1, 0},
+            {0, 1, 1, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+    },
+    // Tetromino Shape S
+    {
+            {0, 1, 1, 0},
+            {1, 1, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+    },
+    // Tetromino Shape T
+    {
+            {0, 1, 0, 0},
+            {1, 1, 1, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+    },
+    // Tetromino Shape Z
+    {
+            {1, 1, 0, 0},
+            {0, 1, 1, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+    }
+};
+
+Color TetrominoColors[TOTAL_TETROMINOES + 1] = {
+    SKYBLUE,    // I
+    DARKBLUE,   // J
+    ORANGE,     // L
+    YELLOW,     // O
+    GREEN,      // S
+    PURPLE,     // T
+    RED,        // Z
+    CUSTOM_COLOR_GAME_PLAYFIELD
+};
+
+Tetromino_t *Tetromino_Create(const Game *game, const TetrominoShape shape) {
+    Tetromino_t *tetromino = arena_alloc_type(game->arena, Tetromino_t);
+    if (tetromino == NULL) {
+        fprintf(stderr, "Unable to allocate space for tetromino.\n");
+        return NULL;
+    }
+
+    for (int i = 0; i < TETROMINO_SIZE; i++) {
+        for (int j = 0; j < TETROMINO_SIZE; j++) {
+            if (tetromino_shape[shape][i][j] == 1) {
+                tetromino->grid[i][j] = shape;
+            } else {
+                tetromino->grid[i][j] = EMPTY_TETROMINO;
+            }
+        }
+    }
+
+    tetromino->rotation_state = DEGREE_0;
+    tetromino->shape = shape;
+    tetromino->color = TetrominoColors[shape];
+    Tetromino_Rotate(tetromino, CW);
+    tetromino->pos = (Vector2i) { (GRID_WIDTH - TETROMINO_SIZE - 1) / 2, 0 };
+
+    return tetromino;
+}
+
+void Tetromino_Rotate(Tetromino_t *tetromino, const TetrominoDirection direction) {
+    int rotated_grid[TETROMINO_SIZE][TETROMINO_SIZE] = { 0 };
+    switch (direction){
+        case CW:
+            for (int i = 0; i < TETROMINO_SIZE; i++) {
+                for (int j = 0; j < TETROMINO_SIZE; j++) {
+                    rotated_grid[j][TETROMINO_SIZE - i - 1] = tetromino->grid[i][j];
+                }
+            }
+            tetromino->rotation_state = (tetromino->rotation_state + 1) % TOTAL_ROTATION_STATES;
+            break;
+        case CCW:
+            for (int i = 0; i < TETROMINO_SIZE; i++) {
+                for (int j = 0; j < TETROMINO_SIZE; j++) {
+                    rotated_grid[TETROMINO_SIZE - j - 1][i] = tetromino->grid[i][j];
+                }
+            }
+            tetromino->rotation_state = (tetromino->rotation_state - 1 + TOTAL_ROTATION_STATES) % TOTAL_ROTATION_STATES;
+            break;
+        default:
+            // This should never happen
+            break;
+    }
+
+    memcpy(tetromino->grid, rotated_grid, sizeof(tetromino->grid));
+}
+
+void Tetromino_Move(Tetromino_t *tetromino) {
+
+}
