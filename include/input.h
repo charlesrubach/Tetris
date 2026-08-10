f0a4a30e8051871296fd013dc2048c7f200f4eef
Charles Rubach <charlesrubach@gmail.com>
2026-08-10 10:58:25 -0500

    Created Input struct type

--- include/input.h
+++ include/input.h
@@ -4,5 +4,16 @@
 
 #ifndef TETRIS_INPUT_H
 #define TETRIS_INPUT_H
+#include <stdbool.h>
+
+typedef struct Input {
+    double das;
+    double arr;
+    double last_move_time;
+    double move_interval;
+    bool left_held;
+    bool right_held;
+    bool rotation;
+} Input;
 
 #endif //TETRIS_INPUT_H
